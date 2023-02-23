cee671bd4a226b0857b14d35fafa76c6a763cd17
xBlood37 <123d.123b@mail.ru>
2023-02-23 14:17:45 +0300

    ref

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const openPopupButton = document.querySelector('.menu__item_open');
-const closePopupButton = document.querySelector('.popup-menu__close');
-const popupAside = document.querySelector('.aside');
+// const openPopupButton = document.querySelector('.menu__item_open');
+// const closePopupButton = document.querySelector('.popup-menu__close');
+// const popupAside = document.querySelector('.aside');
 
-function blockOnDisplay(value) {
-  popupAside.style.display = value;
-}
+// function blockOnDisplay(value) {
+//   popupAside.style.display = value;
+// }
 
-closePopupButton.addEventListener('click', () => {
-  blockOnDisplay('none');
-});
+// closePopupButton.addEventListener('click', () => {
+//   blockOnDisplay('none');
+// });
 
-openPopupButton.addEventListener('click', () => {
-  blockOnDisplay('flex');
-});
+// openPopupButton.addEventListener('click', () => {
+//   blockOnDisplay('flex');
+// });
