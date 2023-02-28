aa631a252d3afe4973ed1fbbaa631011efd7c2c9
xBlood37 <123d.123b@mail.ru>
2023-02-28 12:51:44 +0300

    show all rer

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 // const openPopupButton = document.querySelector('.menu__item_open');
 // const closePopupButton = document.querySelector('.popup-menu__close');
 // const popupAside = document.querySelector('.aside');
-
-// function blockOnDisplay(value) {
-//   popupAside.style.display = value;
-// }
-
-// closePopupButton.addEventListener('click', () => {
-//   blockOnDisplay('none');
-// });
-
-// openPopupButton.addEventListener('click', () => {
-//   blockOnDisplay('flex');
-// });
+const moreButton = document.querySelector('.more');
+const moreSpan = document.querySelector('.more__span');
+const swiperSlide1 = document.querySelector('#swiper-card-block-1');
+const swiperSlide2 = document.querySelector('#swiper-card-block-2');
 
 new Swiper('.swiper', {
   pagination: {
@@ -32,4 +24,28 @@ new Swiper('.swiper', {
       spaceBetween: 24,
     },
   },
+});
+
+// function blockOnDisplay(value) {
+//   popupAside.style.display = value;
+// }
+
+// closePopupButton.addEventListener('click', () => {
+//   blockOnDisplay('none');
+// });
+
+// openPopupButton.addEventListener('click', () => {
+//   blockOnDisplay('flex');
+// });
+
+moreButton.addEventListener('click', () => {
+  if (moreSpan.textContent === 'Скрыть') {
+    moreSpan.textContent = 'Показать все';
+    swiperSlide1.style.display = 'none';
+    swiperSlide2.style.display = 'none';
+  } else if (moreSpan.textContent === 'Показать все') {
+    moreSpan.textContent = 'Скрыть';
+    swiperSlide1.style.display = 'block';
+    swiperSlide2.style.display = 'block';
+  }
 });
